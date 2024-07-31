0e2ac1c5f414551246323473b88c59eb8636d72e
neaguolt <neaguolt@gmail.com>
2024-07-31 11:48:39 +0300

    Add ex06 ft_str_is_printable

---
+++ ex06/ft_str_is_printable.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: deb <neaguolt@gmail.com>                   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/31 11:38:35 by deb               #+#    #+#             */
+/*   Updated: 2024/07/31 11:48:02 by deb              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_str_is_printable(char *str);
+
+int	ft_str_is_printable(char *str)
+{
+	if (str)
+	{
+		if (str[0] == '\0')
+		{
+			return (1);
+		}
+		while (*str)
+		{
+			if ((*str < 32) || (*str > 126))
+			{
+				return (0);
+			}
+			str++;
+		}
+		return (1);
+	}
+	return (0);
+}
+
+int	main(void)
+{
+	char	*str1;
+
+	str1 = "ABC#$%asd123-";
+	if (ft_str_is_printable(str1))
+	{
+		write(1, "String is_printable\n", 21);
+	}
+	else
+	{
+		write(1, "Other String\n", 13);
+	}
+}
--- readme.md
+++ readme.md
@@ -88,17 +88,19 @@ int ft_str_is_lowercase(char *str);
 
 
 Exercise 05 : ft_str_is_uppercase
-Exercise 05
-ft_str_is_uppercase
+
 Turn-in directory : ex05/
 Files to turn in : ft_str_is_uppercase.c
 Allowed functions : None
+
 • Create a function that returns 1 if the string given as a parameter contains only
 uppercase alphabetical characters, and 0 if it contains any other character.
 • Here’s how it should be prototyped :
+
 int ft_str_is_uppercase(char *str);
+
 • It should return 1 if str is empty.
-11
+
 Chapter IX
 Exercise 06 : ft_str_is_printable
 Exercise 06
